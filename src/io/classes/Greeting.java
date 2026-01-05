bc7e689e6dbca2908f4773d76b54ae59eb0e4cb9
Anderson Battisti <anderson.battisti@universo.univates.br>
2026-01-04 22:55:27 -0300

    java.io first example

---
+++ src/io/classes/Greeting.java
@@ -0,0 +1,19 @@
+package io.classes;
+
+public class Greeting
+{
+    static void main()
+    {
+        int num1 = 104;
+        int num2 = 101;
+        int num3 = 108;
+        int num4 = 111;
+ 
+        char letter1 = (char) num1; // 'h'
+        char letter2 = (char) num2; // 'e'
+        char letter3 = (char) num3; // 'l'
+        char letter4 = (char) num4; // 'o'
+ 
+        System.out.println( "Greeting: " + letter1 + letter2 + letter3 + letter3 + letter4 ); 
+    }
+}
