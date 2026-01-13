85c9b630ad88c7d9102eca30373dbde53a9a34db
Anderson Battisti <anderson.battisti@universo.univates.br>
2026-01-12 23:20:44 -0300

    IMC calculator project started, header done

--- src/projects/imc/Main.java
+++ src/projects/imc/Main.java
@@ -1,9 +1,25 @@
 package projects.imc;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class Main
+    extends
+        Application
 {
-    static void main()
+    static void main( String[] args )
     {
-        // TODO
+        launch( args );
+    }
+    
+    @Override
+    public void start( Stage stage )
+    {
+        Scene scene = new Scene( new IMCCalculatorPane(), 500, 400 );
+        
+        stage.setTitle( "IMC Calculator" );
+        stage.setScene( scene );
+        stage.show();
     }
 }
