c2f5b6484af35e6fd5e01bbd7eeef5f6c7c2ba39
Anderson Battisti <anderson.battisti@universo.univates.br>
2026-02-12 23:05:03 -0300

    Weather application: Content layer created, added a label and the search field with icon, the screen layout is still being projected and built

--- src/projects/real/time/weather/api/Main.java
+++ src/projects/real/time/weather/api/Main.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class Main
     extends
@@ -20,8 +19,8 @@ public class Main
     {
         Scene scene = new Scene( new WeatherApplicationPane() );
         
-        stage.setMaxHeight(  Double.MAX_VALUE );
-        stage.setMaxWidth(  Double.MAX_VALUE );
+        stage.setMaxHeight( Double.MAX_VALUE );
+        stage.setMaxWidth( Double.MAX_VALUE );
         stage.setTitle( "Weather Application" );
         stage.getIcons().add( new Image( getClass().getResourceAsStream( "/weather.application/application_icon.png" ) ) );
         stage.setMaximized( true );
--- src/projects/real/time/weather/api/WeatherApplicationPane.java
+++ src/projects/real/time/weather/api/WeatherApplicationPane.java
@@ -1,12 +1,19 @@
 package projects.real.time.weather.api;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 
@@ -14,6 +21,8 @@ public class WeatherApplicationPane
     extends
         StackPane
 {
+    private BorderPane content = new BorderPane();
+    
     public WeatherApplicationPane()
     {
         initComponents();
@@ -23,6 +32,7 @@ public class WeatherApplicationPane
     {
         initBackground();
         initOverlay();
+        initContent();
     }
     
     private void initBackground()
@@ -45,5 +55,70 @@ public class WeatherApplicationPane
         overlay.setStyle(  "-fx-background-color: rgba( 0, 0, 0, 0.60 );" );
         
         getChildren().add( overlay );
+    }
+    
+    private void initContent()
+    {
+        content.setPadding( new Insets( 40 ) );
+        content.setStyle("-fx-border-color: yellow;");
+        
+        content.setTop( createHeader() );
+        
+        getChildren().add( content );
+    }
+    
+    private Node createHeader()
+    {
+        StackPane headerRoot = new StackPane();
+        headerRoot.setPadding( new Insets( 20 ) );
+        headerRoot.setStyle("-fx-border-color: red;");
+        
+        HBox appTitleContainer = new HBox();
+        appTitleContainer.setAlignment( Pos.CENTER_LEFT );
+        StackPane.setAlignment( appTitleContainer, Pos.CENTER_LEFT );
+        
+        Label appTitle = new Label( "Weather Application" );
+        appTitle.setStyle( " -fx-text-fill: white; -fx-font-size: 22px; -fx-font-weight: bold;" );
+     
+        appTitleContainer.getChildren().add( appTitle );
+        
+        headerRoot.getChildren().addAll( appTitleContainer, /*searchFieldContainer*/getSearchFieldComponent( headerRoot ) );
+        
+        return headerRoot;
+    }
+    
+    private Node getSearchFieldComponent( StackPane headerRoot )
+    {
+        StackPane searchFieldContainer = new StackPane();
+        searchFieldContainer.setMaxWidth( Double.MAX_VALUE );
+        searchFieldContainer.maxWidthProperty().bind( headerRoot.widthProperty().multiply( 0.2 ) );
+        searchFieldContainer.setStyle( "-fx-border-color: blue;" );
+        
+        ImageView icon = new ImageView( new Image( getClass().getResourceAsStream( "/weather.application/search_icon.png" ) ) );
+        icon.setFitWidth( 25 );
+        icon.setFitHeight( 25 );
+        icon.setPreserveRatio( true );
+        
+        TextField searchField = new TextField();
+        searchField.setPromptText( "Search city..." );
+        searchField.maxWidthProperty().bind( headerRoot.widthProperty().multiply( 0.2 ) );
+        searchField.setStyle( """
+                              -fx-background-color: rgba(255,255,255,0.1); 
+                              -fx-text-fill: white; 
+                              -fx-prompt-text-fill: rgba(255,255,255,0.6); 
+                              -fx-background-radius: 8; 
+                              -fx-padding: 10 10 10 40; 
+                              -fx-border-color: green; 
+                              -fx-background-radius: 20; 
+                              -fx-border-radius: 20;
+                              """ );
+        
+        StackPane.setAlignment( searchField, Pos.CENTER );
+        StackPane.setAlignment( icon, Pos.CENTER_LEFT );
+        StackPane.setMargin( icon, new Insets( 0, 0, 0, 10 ) );
+        
+        searchFieldContainer.getChildren().addAll( searchField, icon );
+        
+        return searchFieldContainer;
     }
 }
