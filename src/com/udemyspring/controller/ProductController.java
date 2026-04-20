2051dbecc117d6a49453952f7451dee67fe3e02d
Anderson Battisti <anderson.battisti@universo.univates.br>
2026-04-19 22:56:42 -0300

    Simple html page created to fetch the data in the spring server side to show on the browser and changed api cors to allow the local requisitions

--- src/com/udemyspring/controller/ProductController.java
+++ src/com/udemyspring/controller/ProductController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import com.udemyspring.model.Product;
 import com.udemyspring.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin( origins = "*" )
 @RestController
 @RequestMapping( "/products" )
 public class ProductController
